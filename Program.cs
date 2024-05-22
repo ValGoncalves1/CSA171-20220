2e363de3f5e9b7f58f99bc8ad1840d7b7e79a705
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 21:50:08 -0300

    Lista1Ex1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoMatheus1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int area;
+            string aux;
+
+            Console.WriteLine("digite o valor do retangulo:");
+            aux = Console.ReadLine();
+            b = int.Parse(aux);
+
+            Console.WriteLine("digite o valor do retangulo:");
+            a = int.Parse(Console.ReadLine());
+
+            area = b + a;
+
+            Console.WriteLine("o valor do retangulo e:");
+            Console.WriteLine(area);
+        }
+    }
+}
