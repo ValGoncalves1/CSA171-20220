ac9d1756d0c05a4aaf8a284ec2a41956027cb1fc
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:01:37 -0300

    Add files via upload

---
+++ Lista1Ex3/Lista1Ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoMatheus2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int aresta;
+            int area;
+            string aux;
+
+            Console.WriteLine("digite o valor da aresta:");
+            aux = Console.ReadLine();
+            aresta = int.Parse(aux);
+
+            area = aresta * aresta;
+
+
+
+            Console.WriteLine("o valor da area do quadrado é:");
+            Console.WriteLine(area);
+
+        }
+    }
+}
