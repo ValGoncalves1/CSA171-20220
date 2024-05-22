7c8ee07de2d8e0f59b5a606494c3751b596d20ed
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:06:45 -0300

    Add files via upload

---
+++ Lista1Ex10/Lista1Ex10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace milha_maritima
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const double milhaMaritimaEmMetros = 1852; // uma milha marítima equivale a 1852 metros
+            const double metroEmQuilometro = 0.001; // um quilômetro equivale a 1000 metros
+
+            Console.WriteLine("Digite o número de milhas marítimas:");
+            double milhasMaritimas = Convert.ToDouble(Console.ReadLine());
+
+            double metros = milhasMaritimas * milhaMaritimaEmMetros;
+
+            double quilometros = metros * metroEmQuilometro;
+
+            Console.WriteLine("{0} milhas marítimas equivalem a {1} quilômetros.", milhasMaritimas, quilometros);
+        }
+    }
+}
