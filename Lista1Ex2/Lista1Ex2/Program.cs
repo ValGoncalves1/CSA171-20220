3ced95df87a45b6f237f64cf9c0bb05bdb0cecd1
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 21:57:07 -0300

    Add files via upload

---
+++ Lista1Ex2/Lista1Ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTrinangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            int area;
+            string aux;
+
+            Console.WriteLine("Digite o numero da base 'a'");
+            aux = Console.ReadLine();
+            a = int.Parse(aux);
+
+            Console.WriteLine("Digite o numero da base 'b'");
+            b = int.Parse(Console.ReadLine());
+
+            area = a * b / 2;
+
+            Console.WriteLine("a area do triangulo é:");
+            Console.WriteLine(area);
+        }
+    }
+}
