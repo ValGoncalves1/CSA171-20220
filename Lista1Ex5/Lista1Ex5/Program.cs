496d410293f106def0fa1f0de0e924e5dc01d2d1
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:03:07 -0300

    Add files via upload

---
+++ Lista1Ex5/Lista1Ex5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagonal_retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int diagonal;
+            int lado;
+            int area;
+            string aux;
+
+            Console.WriteLine("digite o valor da diagonal do quadrado:");
+            aux = Console.ReadLine();
+            diagonal = int.Parse(aux);
+
+
+            lado = (int)(diagonal / Math.Sqrt(2));
+
+            area = lado * lado;
+
+            Console.WriteLine("A area diagonal do quadrado é:");
+            Console.WriteLine(area);
+        }
+    }
+}
