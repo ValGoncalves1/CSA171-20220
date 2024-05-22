5dcf5192ec659b3354092dc7b670a7a5450af769
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:02:26 -0300

    Add files via upload

---
+++ Lista1Ex4/Lista1Ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cotação_dolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cotacaoDolar;
+            double valorDolares;
+            double valorReais;
+
+            Console.WriteLine("Digite a cotação do dólar:");
+            cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a quantidade de dólares:");
+            valorDolares = Convert.ToDouble(Console.ReadLine());
+
+            valorReais = cotacaoDolar * valorDolares;
+
+            Console.WriteLine("O valor de {1} dolares corresponde a {0} Reais", valorReais, valorDolares);
+        }
+    }
+}
