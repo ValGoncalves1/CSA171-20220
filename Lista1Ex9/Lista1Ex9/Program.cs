44700439e367469a05a6e8b243ad9147524e0b91
ValGoncalves1 <valgs27@gmail.com>
2024-05-21 22:06:12 -0300

    Add files via upload

---
+++ Lista1Ex9/Lista1Ex9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conversor_celcius
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            double fahrenheit;
+
+
+            Console.WriteLine("Digite a temperatura em graus Celsius:");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine("{0} graus Celsius equivalem a {1} graus Fahrenheit.", celsius, fahrenheit);
+        }
+    }
+}
